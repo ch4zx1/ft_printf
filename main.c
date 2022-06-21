a56ca85a19974a4831016b4af5add1874b8fe920
ch4zx1 <charles.fischer17@gmail.com>
2022-06-22 00:54:11 +0200

    checker

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cfischer <cfischer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 17:37:55 by cfischer          #+#    #+#             */
-/*   Updated: 2022/06/21 03:35:05 by cfischer         ###   ########.fr       */
+/*   Updated: 2022/06/22 00:53:42 by cfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,33 @@
 
 #include <stdio.h> 
 
-int checker(int flag, va_list ap)
+long int inverter(long int num)
 {
-	int c;
+	long int res;
+	
+	res = 0;
+
+	while(num / 10)
+	{
+		res += num % 10;
+		res = res * 10;
+		num = num / 10;
+	}
+	res += num;
+	return res;
+}
+
+int checker_str(int flag, va_list ap)
+{
 	char* s;
 	int i;
 
+	i = 0;
+	
 	if(flag == 'c')
 	{
 		i = 1;
-		c = va_arg(ap, int);
-		ft_putchar_fd(c, 1);
+		ft_putchar_fd(va_arg(ap, int), 1);
 	}
 	if(flag == 's')
 	{
@@ -38,6 +54,40 @@ int checker(int flag, va_list ap)
 	{
 		i = 1;
 		ft_putchar_fd('%', 1);
+	}
+	return i;
+}
+
+int checker_num(int flag, va_list ap)
+{
+	int uepa;
+	long int ui;
+	int i;
+	char* s;
+
+	i = 0;
+	
+	if(flag == 'd' || flag == 'i')
+	{
+		s = ft_itoa(va_arg(ap, int));
+		i = ft_strlen(s);
+		ft_putstr_fd(s, 1);
+	}
+	if(flag == 'u')
+	{
+		ui = inverter(va_arg(ap, unsigned int));
+		
+		while (ui > 0)
+		{
+			uepa = ui % 10;
+			ui = ui / 10;
+
+			s = ft_itoa(uepa);
+			i++;
+			ft_putstr_fd(s, 1);
+			free(s);
+		}
+		
 	}
 	return i;
 }
@@ -57,7 +107,8 @@ int ft_printf(const char* str, ...)
 		if (*str == '%')
 		{
 			str++;
-			i += checker(*str, ap);
+			i += checker_str(*str, ap);
+			i += checker_num(*str, ap);
 			str++;
 		}
 		if (*str != '%')
@@ -76,9 +127,13 @@ int main()
 {
 	char* elegosta = "pao de queijo %s  \n"; 
 	
-	printf("%d \n", printf(elegosta, "paozin"));
-
-	printf("%d \n", ft_printf(elegosta, "paozin"));
+	//printf("%d \n", printf(elegosta, "paozin"));
+	
 	//ft_printf(elegosta, 'a', 'd');
 
+	printf("%d \n", ft_printf("Repolho: %u \n", -2));
+	printf("%d \n", printf("Repolho: %u \n", -2));
+	
+	//ft_printf("%u \n", -2);
+	  // printf("Repolho: %c %d %i \n", 'a', 295, 2);
 }
